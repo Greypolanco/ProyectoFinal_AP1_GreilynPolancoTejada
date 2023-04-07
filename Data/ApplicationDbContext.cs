16fd80b3d589b15883f2f95e72a289697caa3d83
Greilyn Polanco <greilyntejada03@gmail.com>
2023-04-06 23:29:21 -0400

    agregando el ModelCreating

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,4 +15,42 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Categorias> Categorias { get; set; }
     public DbSet<Proveedores> Proveedores { get; set; }
     public DbSet<Compras> Compras { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Categorias>().HasData(
+            new Categorias{
+                CategoriaId = 1,
+                Nombre ="Papeleria",
+                Total = 0
+            },
+            new Categorias{
+                CategoriaId =2,
+                Nombre = "Escritura",
+                Total = 0
+            },
+            new Categorias{
+                CategoriaId = 3,
+                Nombre = "Arte Y Manualidad",
+                Total = 0
+            },
+            new Categorias{
+                CategoriaId = 4,
+                Nombre ="Libros",
+                Total = 0
+            },
+            new Categorias{
+                CategoriaId = 5,
+                Nombre = "Suministros de Oficina",
+                Total = 0
+            },
+            new Categorias{
+                CategoriaId = 6,
+                Nombre = "Electronica",
+                Total = 0
+            }
+        );
+    }
 }
