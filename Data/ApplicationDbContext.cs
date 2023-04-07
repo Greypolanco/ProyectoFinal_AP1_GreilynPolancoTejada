995426339af91192f54233d01463e96737a9cbf1
Greilyn Polanco <greilyntejada03@gmail.com>
2023-04-06 23:27:47 -0400

    agregando los modelos al contexto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,8 +5,14 @@ namespace ProyectoFinal_AP1_GreilynPolancoTejada.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    #nullable disable
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Categorias> Categorias { get; set; }
+    public DbSet<Proveedores> Proveedores { get; set; }
+    public DbSet<Compras> Compras { get; set; }
 }
